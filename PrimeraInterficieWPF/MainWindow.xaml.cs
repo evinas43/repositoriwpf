4bdf4daf5ed2da0d734fa587de0e59432f723b9b
ㅤeeriic <evinas.dam@institutcampalans.net>
2025-10-01 19:33:47 +0200

    boton colores

--- PrimeraInterficieWPF/MainWindow.xaml.cs
+++ PrimeraInterficieWPF/MainWindow.xaml.cs
@@ -23,6 +23,55 @@ namespace PrimeraInterficieWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid grid = new Grid();
+            this.Content = grid;
+
+            Button btn = new Button();
+
+            btn.Click += Btn_Click;
+            //grid.Children.Add(btn);
+
+            WrapPanel wrapPanel = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = "hola ";
+            txt1.Background = Brushes.Green;
+
+            wrapPanel.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "adeu ";
+            txt2.Background = Brushes.Cyan;
+            wrapPanel.Children.Add(txt2);
+
+            TextBlock txt3  = new TextBlock();
+            txt3.Text = "buenas ";
+            txt3.Background = Brushes.Red;
+            wrapPanel.Children.Add(txt3);
+            btn.Height = 250;
+            btn.Width = 150;
+            btn.Content = wrapPanel;
+
+
+            grid.Children.Add(btn);
+            
+
+
+
+          
+
+
         }
+          private void Btn_Click (object sender, RoutedEventArgs e)
+        {
+            if(this.Background == Brushes.Red)
+            {
+                this.Background= Brushes.White;
+            }
+            else {this.Background = Brushes.Red; }       
+
+        }
+
     }
 }
